13abcfd45edca851c73dc72348735fe63de6f367
Matias Ludueña <matiasluduena23@gmail.com>
2023-10-19 10:19:30 -0300

    first release

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -2,7 +2,7 @@
 
 // hamburger menu control
 const hamburger = document.querySelector('.hamburger');
-console.log(hamburger)
+
 hamburger?.addEventListener('click', () => {
     console.log('hola')
     const isExpanded = JSON.parse(hamburger.getAttribute('aria-expanded')!);
@@ -14,3 +14,18 @@ window.addEventListener('resize', () =>{
         hamburger?.setAttribute('aria-expanded' ,'false')
     }
 })
+
+ const navbar = document.querySelector('.navbar')
+ const header = document.querySelector('.header')
+
+ const intersection = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        !entry.isIntersecting ? 
+        navbar?.classList.add('scrolled') :
+        navbar?.classList.remove('scrolled')
+    })
+ }, {rootMargin: "-300px 0px 0px 0px"})
+
+if(header){
+     intersection.observe(header)
+}
